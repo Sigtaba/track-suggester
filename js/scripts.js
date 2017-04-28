42b9adcba93c5e5a8152d30538ba16d43cde5ad5
Isabella Abatgis <abatgis@gmail.com>
2017-04-28 11:51:04 -0700

    add javascript conditions and additional content

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+  $("form#coursetracker").submit(function(event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var color = $("#color").val();
+    var environment = $("#environment").val();
+    var type = $("#type").val();
+    var language = $("#language").val();
+
+    if (type === 'front') {
+    $('#css').show();
+    $('#ruby').hide();
+    $('#php').hide();
+    $('#java').hide();
+    $('#csharp').hide();
+  } else if ((environment === 'big') && (type === 'app')) {
+    $('#css').hide();
+    $('#ruby').hide();
+    $('#php').hide();
+    $('#java').show();
+    $('#csharp').hide();
+  } else if ((environment === 'big') && (type === 'back')) {
+    $('#css').hide();
+    $('#ruby').hide();
+    $('#php').hide();
+    $('#java').hide();
+    $('#csharp').show();
+  } else if ((environment === 'small') && (type === 'app')) {
+    $('#css').hide();
+    $('#ruby').show();
+    $('#php').hide();
+    $('#java').hide();
+    $('#csharp').hide();
+  } else if ((environment === 'small' ) && (type === 'back')) {
+    $('#css').hide();
+    $('#ruby').hide();
+    $('#php').show();
+    $('#java').hide();
+    $('#csharp').hide();
+    }
+
+    $('.name').text(name);
+  })
+});
